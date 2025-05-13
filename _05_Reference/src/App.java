b07b94f3f8e92f5f1d0e80a9b1f16abe045f5c35
ssedpspjm <ssedpspjm@gmail.com>
2025-05-13 11:39:01 +0900

    Initial commit

--- _05_Reference/src/App.java
+++ _05_Reference/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Primitive Data Types(기본 자료형)
-        // 스택 메모리에 위치, 일저 크기
+        // 스택 메모리에 위치, 일정 크기
         byte b = 10; // 1byte 숫자
         char c = 'A'; // 2byte 문자(한글자)
         short s = 10000; // 2byte 정수
@@ -10,7 +10,7 @@ public class App {
         float f = 3.14f; // 4byte 실수
         double d = 3.14; // 8byte 실수
 
-        System.out.printf("byte size : %d bit\n ", Byte.SIZE);
+        System.out.printf(" byte size : %d bit\n ", Byte.SIZE);
         System.out.printf("byte size : %d bit\n ", Character.SIZE);
         System.out.printf("byte size : %d bit\n ", Short.SIZE);
         System.out.printf("byte size : %d bit\n ", Integer.SIZE);
